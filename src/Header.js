cdbf2d71915da1954a56c2d6b2a4cba5f4b231ab
John Cruz <jbcruz0209@gmail.com>
2019-10-16 20:40:06 -0700

    fixed the bugs and now working properly

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react'
 
-export default function Header() {
-  return (
-    <header>
-        <h1>ELF Computing</h1>
-        <h3>Laptops</h3>
-        <h5>Customize your laptop</h5>
-    </header>
-  )
+export default function Header() {
+  return (
+    <header>
+        <h1>ELF Computing</h1>
+        <h3>Laptops</h3>
+        <h5>Customize your laptop</h5>
+    </header>
+  )
 }
